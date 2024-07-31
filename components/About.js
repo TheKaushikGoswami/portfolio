b728cd9dc54897ef23a064f7690c1cf4d8d50583
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2024-07-31 15:00:50 +0530

    edit 2

--- components/About.js
+++ components/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About = () => (
   <section>
     <h2>About Me</h2>
+    <img src="/img/science.png" alt="Science" />
     <p>This is the about section.</p>
   </section>
 );
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 const Navbar = () => (
   <nav>
     <ul>
-      <li><Link href="/"><a>Home</a></Link></li>
-      <li><Link href="/about"><a>About</a></Link></li>
-      <li><Link href="/skills"><a>Skills</a></Link></li>
-      <li><Link href="/projects"><a>Projects</a></Link></li>
-      <li><Link href="/contact"><a>Contact</a></Link></li>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/about">About</Link></li>
+      <li><Link href="/skills">Skills</Link></li>
+      <li><Link href="/projects">Projects</Link></li>
+      <li><Link href="/contact">Contact</Link></li>
     </ul>
   </nav>
 );
